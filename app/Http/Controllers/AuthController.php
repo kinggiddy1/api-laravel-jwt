fe64b415200ad7b6a60bcf332e508a5bd07f9cb1
kinggiddy1 <kinggiddy@gmail.com>
2025-01-01 12:10:48 +0200

    added role

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,6 +23,7 @@ class AuthController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => $request->role ?? 'User', 
         ]);
 
         $token = JWTAuth::fromUser($user);
