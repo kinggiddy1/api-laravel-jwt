40861baa74c4ff8514890a7027ae766213cd1b79
kinggiddy1 <kinggiddy@gmail.com>
2025-01-09 13:58:24 +0200

    changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,7 +37,7 @@ class User extends Authenticatable implements JWTSubject
         'email',
         'password',
         'role',
-    ];  
+    ]; 
 
     /**
      * The attributes that should be hidden for serialization.
