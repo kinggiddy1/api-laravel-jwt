271d94980ee1a9580936d5d72f1e58405c02546c
kinggiddy1 <kinggiddy@gmail.com>
2025-01-12 21:48:26 +0200

    changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable implements JWTSubject
     protected $hidden = [
         'password',
         'remember_token',
-    ];  
+    ]; 
 
     /**
      * Get the attributes that should be cast.
