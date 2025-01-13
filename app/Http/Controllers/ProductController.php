ffc56ec1c2373fa2552932b88b5179fd5c124fd8
kinggiddy1 <kinggiddy@gmail.com>
2025-01-13 17:00:49 +0200

    added controller in API route

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\validator;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all(); // Corrected with uppercase 'P'
+        return response()->json(['products' => $products], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+            'quantity' => 'required|integer',
+        ]);
+
+        $products = product::create($request);
+   
+
+        return response()->json(['products' => $products], 201);
+    
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -20,4 +21,5 @@ Route::post('userlist/{id}/', [AuthController::class, 'show']);
 Route::post('delete/{id}/', [AuthController::class, 'delete']);
 
 // PRODUCTS
-Route::get('products', [ProductController::class, 'products']);
+Route::get('products', [ProductController::class, 'index']);
+Route::post('store', [ProductController::class, 'store']);
